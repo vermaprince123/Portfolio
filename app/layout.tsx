0009872b3079b60337fd2a39d04e06bb623d1e22
prince.verma <prince.verma@oneassist.in>
2026-01-24 12:41:27 +0530

    added vercel analytics lib

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/lib/theme-provider";
 import { ScrollToTop } from "@/components/scroll-to-top";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -67,6 +68,7 @@ export default function RootLayout({
         >
           {children}
           <ScrollToTop />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
